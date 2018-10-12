fba20157e2394e46448071d56aa3e8ca6739f62a
ricearonin <taylor.a.rice@gmail.com>
2018-10-11 18:42:01 -0700

    cleanup, new functionality for weather app

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const request = require('request');
 const config = require('./config');
+const yargs = require(`yargs`);
 
-var mykey = config.googleAPI;
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true,
+    },
+  })
+  .help()
+  .alias('help', 'h').argv;
 
-console.log(mykey);
+const mykey = config.googleAPI;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request(
   {
-    url:
-      'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=' +
-      mykey,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${mykey}`,
     json: true,
   },
   (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
   },
 );
